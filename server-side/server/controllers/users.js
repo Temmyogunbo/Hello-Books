95a36d5ca4a81652815dd9b824e26e4a8bbc8155
Temmyogunbo <temmyogunbo@gmail.com>
2017-08-09 14:50:01 +0100

    contains users functions

--- server-side/server/controllers/users.js
+++ server-side/server/controllers/users.js
@@ -1,43 +1,78 @@
-const User = require('../models').User;
 
-module.exports = {
+import db from '../models';
+const env = process.env.NODE_ENV || 'development';
+const config = require(`${__dirname}/../config/config.json`)[env];
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const usersController = {
   createUser(req, res) {
-    const firstName = req.body.firstname;
-    const lastName = req.body.lastname;
-    const userName = req.body.username;
-    const passWord = req.body.password;
-    const email = req.body.email;
-    if (!firstName || !lastName || !userName || !passWord || !email) {
-      res.status(401).send('Inavlid logins');
-    }
-    return User
-      .create({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        username: req.body.username,
-        password: req.body.password,
+    req.check('firstName', 'FirstName is required').notEmpty();
+    req.check('userName', 'userName is required').notEmpty();
+    req.check('membership', 'membership is required').notEmpty();
+    req.check('membership', 'membership must either be silver, gold, or platinum').isMember();
+    req.check('email', 'Email is required').notEmpty();
+    req.check('email', 'Please put a valid email').isEmail();
+    req.check('password', 'Password is required').notEmpty();
+    req.check('password', 'Password must be a mininum of 4 character')
+      .isLength(5, 50);
+    const errors = req.validationErrors();
+    if (errors) {
+      res.status(400).json({ errors });
+    } else {
+      db.User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.userName,
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)),
         email: req.body.email,
+        membership: req.body.membership
       })
-      .then(user => res.status(201).send(user))
-      .catch(err => res.status(400).send(err));
+        .then(user => res.status(201).send(user))
+        .catch(err => res.status(400).send(err.errors[0].message));
+    }
   },
+
   findUser(req, res) {
-    return User
-      .findOne({
+    req.check('userName', 'Username is required').notEmpty();
+    req.check('password', 'Password is required').notEmpty();
+
+    const errors = req.validationErrors();
+    if (errors) {
+      res.status(400).json({ errors });
+    } else {
+      return db.User.findOne({
         where: {
-          username: req.body.username
+          userName: req.body.userName
         }
       })
-      .then((user) => {
-        if (!user) {
-          res.status(200).send('You are not registered');
-        }
-        else if (user.password !== req.body.password) {
-          res.status(401).send('Wrong password');
-        }
-        res.status(200).send('Welcome to HelloBooks');
-      })
-      .catch(err => res.status(400).send(err));
-  },
+        .then((user) => {
+          if (!user) {
+            res.status(200).send('You are not registered');
+          } else if (user) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+              const payLoad = ({
+                email: user.email,
+                id: user.id,
+                userName: user.userName,
+                password: user.password,
+                role: user.role
+              });
+              const token = jwt.sign(payLoad, config.secretKey, {
+                expiresIn: 3600 * 24
+              });
+              res.status(201).json({
+                success: true,
+                token,
+              });
+            } else {
+              res.status(401).send('Wrong password');
+            }
+          }
+        })
+        .catch(err => res.status(400).send(err));
+    }
+  }
 };
 
+export default usersController;
