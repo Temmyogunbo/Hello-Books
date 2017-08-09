130d82ed31167e163893c681b4322ef91c9633a9
Temmyogunbo <temmyogunbo@gmail.com>
2017-08-09 14:53:44 +0100

    contains endpoint

--- server-side/server/routes/index.js
+++ server-side/server/routes/index.js
@@ -1,7 +1,8 @@
-const usersController = require('../controllers').users;
-const booksController = require('../controllers').books;
+import usersController from '../controllers/users';
+import booksController from '../controllers/books';
+import bookHistoryController from '../controllers/borrow';
 
-module.exports = (app) => {
+export default (app) => {
   app.post('/api/v1/users/signin', usersController.findUser);
 
   app.post('/api/v1/users/signup', usersController.createUser);
@@ -9,4 +10,8 @@ module.exports = (app) => {
   app.post('/api/v1/books', booksController.createBook);
   app.get('/api/v1/books', booksController.findBooks);
   app.put('/api/v1/books/:bookId', booksController.updateBook);
+
+  app.post('/api/v1/users/:userId/:bookId', bookHistoryController.borrowBook);
+  app.get('/api/v1/users/:userId/books', bookHistoryController.yetToReturn);
+  app.put('/api/v1/users/:userId/:bookId', bookHistoryController.returnBook);
 };
