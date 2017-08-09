b1442d8a0b36c067931f6bfd30e97634784ec965
Temmyogunbo <temmyogunbo@gmail.com>
2017-08-09 14:52:57 +0100

    exports route controllers

--- server-side/server/models/index.js
+++ server-side/server/models/index.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
@@ -11,9 +12,11 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
-    config.database, config.username, config.password, { 
-      host: config.host, dialect: config.dialect
-});
+    config.database, config.username, config.password, {
+      host: config.host,
+      dialect: config.dialect,
+    }
+  );
 }
 
 fs
@@ -32,7 +35,8 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db);
   }
 });
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
