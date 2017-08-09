1df5c141afecde3adf9e9ce11a9391648b952272
Temmyogunbo <temmyogunbo@gmail.com>
2017-08-09 14:47:59 +0100

    contains book function for app

--- server-side/server/controllers/books.js
+++ server-side/server/controllers/books.js
@@ -1,29 +1,29 @@
-const Books = require('../models').Books;
+// //import Book from '../models/book';
+// const Book = require('../models/book');
+import db from '../models';
 
-module.exports = {
+const booksController = {
   createBook(req, res) {
-    const title = req.body.title;
-    const category = req.body.category;
-    const author = req.body.author;
-    const quantity = req.body.quantity;
-    const quantityBorrowed = req.body.quantityBorrowed;
-    if (!title || !category || !author || !quantity || !quantityBorrowed) {
-      res.status(401).send('Inavlid logins details');
-    }
-    return Books
-      .create({
+    const errors = req.validationErrors();
+    req.check('title', 'Title is required').notEmpty();
+    req.check('author', 'Author is required').notEmpty();
+    req.check('category', 'Category is required').notEmpty();
+    req.check('quantity', 'Quantity is required').notEmpty();
+    if (errors) {
+      res.status(400).json({ errors });
+    } else {
+      db.Book.create({
         title: req.body.title,
         category: req.body.category,
         author: req.body.author,
-        quantity: req.body.quantity,
-        quantityBorrowed: req.body.quantityBorrowed,
+        quantity: req.body.quantity
       })
-      .then(book => res.status(201).send(book))
-      .catch(err => res.status(400).send(err));
+        .then(book => res.status(201).send(book))
+        .catch(err => res.status(400).send(err));
+    }
   },
   findBooks(req, res) {
-    return Books
-      .findAll({})
+    return db.Book.findAll({})
       .then((books) => {
         if (books.length === 0) {
           res.status(200).send('No books in the library');
@@ -33,19 +33,33 @@ module.exports = {
       .catch(err => res.status(404).send(err));
   },
   updateBook(req, res) {
-    return Books
-      .findOne({
+    const errors = req.validationErrors();
+    req.check('title', 'Title is required').notEmpty();
+    req.check('author', 'Author is required').notEmpty();
+    req.check('category', 'Category is required').notEmpty();
+    req.check('quantity', 'Quantity is required').notEmpty();
+    if (errors) {
+      res.status(400).json({ errors });
+    } else {
+      return db.Book.update({
+        category: req.body.category,
+        title: req.body.title,
+        author: req.body.author,
+        quantity: req.body.quantity
+      },
+      {
+        fields: ['category', 'title', 'author', 'quantity'],
         where: {
           id: req.params.bookId
         }
       })
-      .then((user) => {
-        user.updateAttributes({
-          category: req.body.category
+        .then(() => {
+          res.status(200).send('Book updated');
         })
-          .then(resp => res.status(200).send(resp));
-      })
-      .catch(err => res.status(400).send(err));
+        .catch((err) => {
+          res.status(405).send(err);
+        });
+    }
   }
 };
-
+export default booksController;
