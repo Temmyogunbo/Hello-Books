9248327cbf3dd0a45132b57d1b4ff7079c0e6506
Temmyogunbo <temmyogunbo@gmail.com>
2017-08-10 18:19:06 +0100

    includes public folder for front end

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import expressValidator from 'express-validator';
+import route from './server/routes';
+
+// const index = require('./server/routes/index');
+
+// creating express application
+const app = express();
+
+// log request to the console;
+app.use(logger('dev'));
+
+// format request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+//
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+	response.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Require our routes into the application.
+(route)(app);
+
+export default app;
