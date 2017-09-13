b5342304484820d5d741ae8a512e1204e56f4ddb
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-09-13 20:06:37 +0100

    Create function component to protect other components

---
+++ client/app/utils/CheckSignedInContainer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import toastr from 'toastr';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+export default (ComposedComponent) => {
+  class CheckSignedInContainer extends React.Component {
+    componentWillMount() {
+      if (!this.props.isAuthenticated) {
+        toastr.error('You need to sign in');
+        this.props.history.replace('/signin');
+      }
+    }
+    componentWillUpdate(nextProps) {
+      if (!nextProps.isAuthenticated) {
+        this.props.history.replace('/signin');
+      }
+    }
+    render() {
+      const { match, dispatch, location, staticContext, rest } = this.props;
+      return (
+        <ComposedComponent {...rest} />
+      );
+    }
+  }
+  CheckSignedInContainer.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    history: PropTypes.object.isRequired
+  };
+  function mapStateToProps(state) {
+    return {
+      isAuthenticated: state.userReducer.isAuthenticated
+    };
+  }
+  return connect(mapStateToProps)(CheckSignedInContainer);
+};
