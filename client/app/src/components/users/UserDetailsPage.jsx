aa719cd4960d4845cad3f4d4edf5ea43e3089be1
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-09-13 20:07:47 +0100

    Create component for users page

---
+++ client/app/src/components/users/UserDetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UsersDetailsHeader from './UsersDetailsHeader';
+
+class UserDetailsPage extends React.Component {
+  render() {
+    return (
+      <UsersDetailsHeader />
+    );
+  }
+}
+export default UserDetailsPage;
