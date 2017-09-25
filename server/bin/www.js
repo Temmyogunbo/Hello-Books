f839656db63f1b50d43ca88cd3b6cdff02dd8eab
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-09-25 23:10:49 +0100

    remove comments from file and unuse module

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,8 +1,6 @@
 // Express setup
 import http from 'http';
 import app from '../config/app';
-import db from '../models';
-import Sequelize from 'sequelize'
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
@@ -10,7 +8,3 @@ app.set('port', port);
 // setup http
 const server = http.createServer(app);
 server.listen(port, console.log('Server running currently on', port));
-
-//db.sequelize.sync().then(() => {
-  //server.listen(port);
-//});
