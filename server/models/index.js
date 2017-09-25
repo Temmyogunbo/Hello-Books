7973bfc3fe46d655e64479a6ba9ac0ba126f2122
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-09-26 00:43:32 +0100

    remove unecessary line of code

--- server/models/index.js
+++ server/models/index.js
@@ -15,13 +15,6 @@ const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
-  /* sequelize = new Sequelize(
-     config.database, config.username, config.password, {
-       host: config.host,
-       dialect: config.dialect,
-     }
-   );
-   */
 } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, {
