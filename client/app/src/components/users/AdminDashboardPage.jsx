d9b809ceb27694c90d9b8421d2f560b75bb67add
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-09-26 00:36:29 +0100

    rename to pages to follow Pascal naming convention

--- client/app/src/components/users/AdminDashboardPage.jsx
+++ client/app/src/components/users/AdminDashboardPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import toastr from 'toastr';
 import PropTypes from 'prop-types';
 import DashboardHead from './DashboardHeader';
 import BookCategories from './BookCategories';
--- client/app/src/components/users/WelcomeMessagePage.jsx
+++ client/app/src/components/users/WelcomeMessagePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
-import UserDetailsHeader from './UsersDetailsHeader';
 
 const WelcomeMessagePage = () =>
   (
