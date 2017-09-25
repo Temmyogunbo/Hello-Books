7fce62b04b227126b3a8243cfad2d39531722c81
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-09-25 23:00:29 +0100

    remove unuse button component

--- client/app/src/components/users/AdminBooks.jsx
+++ client/app/src/components/users/AdminBooks.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import swal from 'sweetalert2';
-import Button from './ButtonComponent';
 
 
 class AdminBooks extends React.Component {
