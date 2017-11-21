3a9a7a8e7e8aaadaef28c7d9df64ffda52232b04
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-11-21 16:56:42 +0100

    resolve conflicts in webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -71,16 +71,15 @@ module.exports = {
       template: path.resolve(`${__dirname}/client/app/index.html`)
     }),
     new webpack.DefinePlugin({
-      process: {
-        env: {
-          NODE_ENV: JSON.stringify('production'),
-          SECRET_KEY: JSON.stringify(process.env.SECRET_KEY),
-          APP_CLOUD_NAME: JSON.stringify(process.env.APP_CLOUD_NAME),
-          APP_API_KEY: JSON.stringify(process.env.APP_API_KEY),
-          APP_API_SECRET: JSON.stringify(process.env.APP_API_SECRET),
-          GOOGLE_CLIENT_ID: JSON.stringify(process.env.GOOGLE_CLIENT_ID)
-        }
+      "process.env": {
+        NODE_ENV: JSON.stringify('production'),
+        SECRET_KEY: JSON.stringify(process.env.SECRET_KEY),
+        APP_CLOUD_NAME: JSON.stringify(process.env.APP_CLOUD_NAME),
+        APP_API_KEY: JSON.stringify(process.env.APP_API_KEY),
+        APP_API_SECRET: JSON.stringify(process.env.APP_API_SECRET),
+        GOOGLE_CLIENT_ID: JSON.stringify(process.env.GOOGLE_CLIENT_ID)
       }
+
     })
   ]
 };
