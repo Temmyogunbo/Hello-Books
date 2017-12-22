b3633352b9cf2314bbbfcd5b1d533d4eca392f50
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-12-22 21:15:57 +0100

    bug(feedback):rename files, add jsdocs def and params

--- client/app/__tests__/components/users/userRecords.spec.jsx
+++ client/app/__tests__/components/users/userRecords.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import UserRecords from '../../../src/components/users/UserRecords';
+import UserRecords from '../../../src/components/Users/UserRecords';
 
 const props = {
   userHistory: [{
