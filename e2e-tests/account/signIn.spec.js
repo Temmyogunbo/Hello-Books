8682a78eddbe1e4f002cd7db44be937024543331
temmyogunbo <Ogunbotemilola@yahoo.com>
2017-12-29 18:32:39 +0100

    bug(feedback):add more e2e tests

--- e2e-tests/account/signIn.spec.js
+++ e2e-tests/account/signIn.spec.js
@@ -25,4 +25,39 @@ module.exports = {
       .pause(3000)
       .end();
   },
+
+  'it should not sign in with wrong password': (client) => {
+    client
+      .url('http://localhost:5000/signin')
+      .waitForElementVisible('body')
+      .waitForElementVisible('#sign-in-form', 1000)
+      .setValue('input[name="userName"]', 'temmy')
+      .setValue('input[name="password"]', 'emmanuel123')
+      .click('#for-google-log-in')
+      .pause(2000)
+      .assert.elementPresent('#toast-container')
+      .assert.containsText(
+        '#toast-container',
+        'Invalid username or password',
+      )
+      .pause(3000)
+      .end();
+  },
+  'it should not sign in with wrong username': (client) => {
+    client
+      .url('http://localhost:5000/signin')
+      .waitForElementVisible('body')
+      .waitForElementVisible('#sign-in-form', 1000)
+      .setValue('input[name="userName"]', 'temmy123')
+      .setValue('input[name="password"]', 'emmanuel')
+      .click('#for-google-log-in')
+      .pause(2000)
+      .assert.elementPresent('#toast-container')
+      .assert.containsText(
+        '#toast-container',
+        'Invalid username or password',
+      )
+      .pause(3000)
+      .end();
+  },
 };
--- e2e-tests/account/signUp.spec.js
+++ e2e-tests/account/signUp.spec.js
@@ -16,4 +16,42 @@ module.exports = {
       .assert.urlEquals('http://localhost:5000/collections')
       .end();
   },
+  'it should not sign up when username already exist': (client) => {
+    client
+      .url('http://localhost:5000/signup')
+      .waitForElementVisible('body')
+      .waitForElementVisible('#for-google-signup', 1000)
+      .setValue('input[name="fullName"]', 'saitama')
+      .setValue('input[name="userName"]', 'saitama')
+      .setValue('input[name="email"]', 'saitama@gmail.com')
+      .setValue('input[name="password"]', 'emmanuel')
+      .setValue('input[name="confirmPassword"]', 'emmanuel')
+      .click('#for-google-signup')
+      .pause(2000)
+      .assert.elementPresent('#toast-container')
+      .assert.containsText(
+        '#toast-container',
+        'Username already exist.',
+      )
+      .end();
+  },
+  'it should not sign up when email has been taken': (client) => {
+    client
+      .url('http://localhost:5000/signup')
+      .waitForElementVisible('body')
+      .waitForElementVisible('#for-google-signup', 1000)
+      .setValue('input[name="fullName"]', 'saitama')
+      .setValue('input[name="userName"]', 'saitama123')
+      .setValue('input[name="email"]', 'saitama@gmail.com')
+      .setValue('input[name="password"]', 'emmanuel')
+      .setValue('input[name="confirmPassword"]', 'emmanuel')
+      .click('#for-google-signup')
+      .pause(5000)
+      .assert.elementPresent('#toast-container')
+      .assert.containsText(
+        '#toast-container',
+        'Email has been taken.',
+      )
+      .end();
+  },
 };
